a8d928def8744dec8859685e92afc15f083e64c4
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-07 23:03:09 -0600

    adjusted refresh button opacity

--- assets/site.js
+++ assets/site.js
@@ -10,7 +10,7 @@
 
 
 let __orbBgStop = null;
-export const JSTC_VERSION = "1.4.8";
+export const JSTC_VERSION = "1.4.9";
 
 export function startOrbBackground(opts = {}) {
 	const reduce = window.matchMedia?.("(prefers-reduced-motion: reduce)")?.matches;
