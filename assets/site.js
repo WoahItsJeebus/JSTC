b423a47ecc6ab525d2a4da27e0851850418d68a9
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-08 16:15:09 -0600

    1.4.16

--- assets/site.js
+++ assets/site.js
@@ -10,7 +10,7 @@
 
 
 let __orbBgStop = null;
-export const JSTC_VERSION = "1.4.15";
+export const JSTC_VERSION = "1.4.16";
 
 export function startOrbBackground(opts = {}) {
 	const reduce = window.matchMedia?.("(prefers-reduced-motion: reduce)")?.matches;
