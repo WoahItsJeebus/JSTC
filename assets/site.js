d9c5045546913c5f3e820b799c3c7bdc803f2cf7
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-02-16 09:32:34 -0600

    1.4.18 - 30 day clutter removal

--- assets/site.js
+++ assets/site.js
@@ -10,7 +10,7 @@
 
 
 let __orbBgStop = null;
-export const JSTC_VERSION = "1.4.17";
+export const JSTC_VERSION = "1.4.18";
 
 export function startOrbBackground(opts = {}) {
 	const reduce = window.matchMedia?.("(prefers-reduced-motion: reduce)")?.matches;
